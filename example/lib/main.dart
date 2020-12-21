957d698d465f96fae69f700572b93c13da6f8157
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2020-12-21 14:37:44 +0100

    Flutter format

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,28 +26,28 @@ class MyApp extends StatelessWidget {
         ),
         body: Container(
             child: CustomScrollView(
-              slivers: [
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                      (context, i) => ListTile(
-                            contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-                            tileColor: tileList[i][1],
-                            title: Skeleton(
-                              style: SkeletonStyle.text,
-                              textColor: tileList[i][0],
-                              parentBackgroundColor: tileList[i][1],
-                              height: 14.0,
-                            ),
-                            subtitle: Text(
-                              'Loaded Text',
-                              style: TextStyle(color: tileList[i][0]),
-                            ),
-                            onTap: () {},
-                          ),
-                      childCount: tileList.length),
-                )
-              ],
-            )),
+          slivers: [
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (context, i) => ListTile(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                        tileColor: tileList[i][1],
+                        title: Skeleton(
+                          style: SkeletonStyle.text,
+                          textColor: tileList[i][0],
+                          parentBackgroundColor: tileList[i][1],
+                          height: 14.0,
+                        ),
+                        subtitle: Text(
+                          'Loaded Text',
+                          style: TextStyle(color: tileList[i][0]),
+                        ),
+                        onTap: () {},
+                      ),
+                  childCount: tileList.length),
+            )
+          ],
+        )),
       ),
       // enable dark mode
       darkTheme: ThemeData(brightness: Brightness.dark),
